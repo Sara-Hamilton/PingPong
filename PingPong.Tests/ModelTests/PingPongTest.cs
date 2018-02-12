cdcd7c95198bbd583faa5ec66c5c584953dea8ad
Nico Daunt and Sara Hamilton <sara@>
2018-02-12 10:51:30 -0800

    create red test condition for ThreeToPing test method

--- PingPong.Tests/ModelTests/PingPongTest.cs
+++ PingPong.Tests/ModelTests/PingPongTest.cs
@@ -14,5 +14,13 @@ namespace PingPong.Tests
       PingPongGenerator testPingPong = new PingPongGenerator();
       CollectionAssert.AreEqual(new List<string>(){"1","2","3","4","5"}, testPingPong.PingPong(5));
     }
+
+    [TestMethod]
+    public void PingPong_ThreeToPing_Array()
+    {
+      PingPongGenerator testPingPong = new PingPongGenerator();
+      CollectionAssert.AreEqual(new List<string>(){"1","2","ping","4","5"}, testPingPong.PingPong(5));
+
+    }
   }
 }
