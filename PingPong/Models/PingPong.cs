bd1877f37e8842e9f6b6395e7940168295f10269
Nico Daunt and Sara Hamilton <sara@>
2018-02-12 11:46:14 -0800

    create green test condition for ThreeAndFiveToPingPong test method

--- PingPong/Models/PingPong.cs
+++ PingPong/Models/PingPong.cs
@@ -10,7 +10,25 @@ namespace PingPong
       List<string> result = new List<string>();
       for (int i = 1; i <= range; i++)
       {
-        result.Add(i.ToString());
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+          string pingPong = "ping-pong";
+          result.Add(pingPong);
+        }
+        else if (i % 3 == 0)
+        {
+          string ping = "ping";
+          result.Add(ping);
+        }
+        else if (i % 5 == 0)
+        {
+          string pong = "pong";
+          result.Add(pong);
+        }
+        else
+        {
+          result.Add(i.ToString());
+        }
       }
       return result;
     }
