bb9780abd94d33d38c71e99f4842abe2624d5a90
Nico Daunt and Sara Hamilton <sara@>
2018-02-12 13:52:52 -0800

    add views and controller

--- PingPong/Controllers/PingPongController.cs
+++ PingPong/Controllers/PingPongController.cs
@@ -1,12 +1,25 @@
 using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
-// using PingPong.Models;
+using PingPong.Models;
 
 namespace PingPong.Controllers
 {
   public class PingPongController : Controller
   {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+    [HttpPost("/")]
+    public ActionResult Display()
+    {
+      PingPongGenerator newPingPongGenerator = new PingPongGenerator();
+      List<string> model = newPingPongGenerator.PingPong(Int32.Parse(Request.Form["range"]));
+
+      return View(model);
+    }
 
   }
 }
